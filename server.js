ca93a263cd987ecaaf477584b43da3f59c190a09
Roger Bloom <rogerbloom1@gmail.com>
2021-03-01 07:03:45 -0800

    exercise

--- server.js
+++ server.js
@@ -1,5 +1,36 @@
 const http = require("http");
 
+http
+.createServer((request, response) => {
+    const { url, method } = request;
+    const chunks = [];
+    request.on("data", (chunk) => {
+        chunks.push(chunk);
+    }).on("end", () => {
+        const body = Buffer.concat(chunks).toString();
+        const responseBody = { url: url, method: method, body: body, };
+    });
+
+    if (url === "/") {
+        response.write("Howdy!");
+    } else if (url === "/about") {
+        response.write("Name: Roger Bloom\nCity: Huntington Beach");
+    } else if (url === "/echo") {
+        response.setHeader("Content-Type", "application/json");
+        response.write(JSON.stringify(responseBody));
+    } else {
+        response.statusCode = 404;
+        resopnse.write("<h1>404 - Not Found</h1>");
+    }
+    return response.end();
+}).listen(3000);
+    
+
+
+
+
+/* const http = require("http");
+
 const html = `<main style="background-color: burlywood; height: 100vh; margin: 0; padding:0;">
     <h1>Raise the Anthem, Burlywood</h1>
 </main>`
@@ -10,7 +41,7 @@ http
         error: "You made a bad request", 
         code: 400, 
     };
-    const { url } = request;
+    const { url, method } = request;
    
     request.on("error", (error) => {
         response.statusCode = 400;
@@ -21,7 +52,16 @@ http
         return response.end();
     });
 
-    request.emit("error");
+    const chunks = [];
+    request.on("data", (chunk) => {
+        chunks.push(chunk);
+    })
+    .on("end", () => {
+        const body = Buffer.concat(chunks).toString()
+        
+        const responseBody = {
+            url: url, method: method, body: body,
+        };
 
     response.on("error", (error) => {
         errorMsg.error = "An error occurred on the server.";
@@ -30,21 +70,23 @@ http
         response.write(JSON.stringify(errorMsg));
         return response.end();
     });
-
+        response.setHeader("Content-Type", "text/html");
     
-
-    response.setHeader("Content-Type", "text/html");
     if (url === "/") {
-        response.write("<h1>Home</h1>");
+        response.write(html);
     } else if (url === "/about") {
         response.write("<h1>About</h1>");
-    } else {
+    } else if (url === "/echo") {
+        response.setHeader("Content-Type", "application/json");
+        response.write(JSON.stringify(responseBody));
+    }else {
         response.statusCode = 404;
         response.write("<h1>404 - Page Not Found</h1>")
     }
     return response.end();
+    });
 })
-.listen(3000, () => console.log("Server listening ..."));
+.listen(3000, () => console.log("Server listening ...")); */
 
 /* http.createServer((request, response) => {
     const { method, url } = request;
